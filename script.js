34b64a94ae49387fe39a9af7f5b8591f6567963a
ArtificerArturo <198648779+ArtificerArturo@users.noreply.github.com>
2025-03-27 16:36:14 -0500

    MVP

--- script.js
+++ script.js
@@ -13,123 +13,122 @@ function calculateUnitCircle() {
         resultElement.style.color = "red";
         return
     }
+
     if (dropdown.value == "degrees") {
         let conditionedAngle = +(eval(angle) % 360).toFixed(3)
+        checkSpecialValues(conditionedAngle)
+        resultElement.innerHTML = `x = cos(${conditionedAngle})° = ${x}\ny = sin(${conditionedAngle})° = ${y}\ntan(${conditionedAngle})° = ${tan}`
+        const coordDiv = document.createElement("div")
+        coordDiv.innerHTML = `(${x}, ${y})`
+        resultElement.appendChild(coordDiv)
+    } else if (dropdown.value == "radians") {
+        let conditionedAngle = +(eval(angle) % (2 * Math.PI)).toFixed(3)
+        checkSpecialValues(conditionedAngle)
+        resultElement.innerHTML = `x = cos(${conditionedAngle}) = ${x}\ny = sin(${conditionedAngle}) = ${y}\ntan(${conditionedAngle}) = ${tan}`
 
+    } else if (dropdown.value == "piRad") {
+        let conditionedAngle = math.fraction(angle).mod(2)
+        checkSpecialValues(conditionedAngle)
+        resultElement.innerHTML = `x = cos(${conditionedAngle}) = ${x}\ny = sin(${conditionedAngle}) = ${y}\ntan(${conditionedAngle}) = ${tan}`
+
+    }
+
+    function checkSpecialValues(conditionedAngle) {
         if (conditionedAngle == 0) {
             x = 1
             y = 0
             tan = 0
-        } else if (conditionedAngle == 30 || conditionedAngle == -330) {
+        } else if ((dropdown.value == "degrees" && (conditionedAngle == 30 || conditionedAngle == -330)) ||
+            (dropdown.value == "piRad") && ((math.equal(conditionedAngle, math.fraction('1/6')))) || math.equal(conditionedAngle, math.fraction('-11/6'))) {
             x = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><msqrt><mn>3</mn></msqrt><mn>2</mn></mfrac></math>`
             y = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mn>1</mn><mn>2</mn></mfrac></math>`
             tan = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><msqrt><mn>3</mn></msqrt><mn>3</mn></mfrac></math>`
-        } else if (conditionedAngle == 45 || conditionedAngle == -315) {
+        } else if ((dropdown.value == "degrees" && (conditionedAngle == 45 || conditionedAngle == -315)) ||
+            (dropdown.value == "piRad") && ((math.equal(conditionedAngle, math.fraction('1/4')))) || math.equal(conditionedAngle, math.fraction('-7/4'))) {
             x = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><msqrt><mn>2</mn></msqrt><mn>2</mn></mfrac></math>`
             y = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><msqrt><mn>2</mn></msqrt><mn>2</mn></mfrac></math>`
             tan = 1
-        } else if (conditionedAngle == 60 || conditionedAngle == -300) {
+        } else if ((dropdown.value == "degrees" && (conditionedAngle == 60 || conditionedAngle == -300)) ||
+            (dropdown.value == "piRad") && ((math.equal(conditionedAngle, math.fraction('1/3')))) || math.equal(conditionedAngle, math.fraction('-5/3'))) {
             x = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mn>1</mn><mn>2</mn></mfrac></math>`
             y = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><msqrt><mn>3</mn></msqrt><mn>2</mn></mfrac></math>`
             tan = `<math xmlns="http://www.w3.org/1998/Math/MathML"><msqrt><mn>3</mn></msqrt></math>`
-        } else if (conditionedAngle == 90 || conditionedAngle == -270) {
+        } else if ((dropdown.value == "degrees" && (conditionedAngle == 90 || conditionedAngle == -270)) ||
+            (dropdown.value == "piRad") && ((math.equal(conditionedAngle, math.fraction('1/2')))) || math.equal(conditionedAngle, math.fraction('-3/2'))) {
             x = 0
             y = 1
             tan = "undefined"
-        } else if (conditionedAngle == 120 || conditionedAngle == -240) {
+        } else if ((dropdown.value == "degrees" && (conditionedAngle == 120 || conditionedAngle == -240)) ||
+            (dropdown.value == "piRad") && ((math.equal(conditionedAngle, math.fraction('2/3')))) || math.equal(conditionedAngle, math.fraction('-4/3'))) {
             x = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mrow><mo>-</mo><mn>1</mn></mrow><mn>2</mn></mfrac></math>`
             y = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><msqrt><mn>3</mn></msqrt><mn>2</mn></mfrac></math>`
             tan = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mo>-</mo><msqrt><mn>3</mn></msqrt></math>`
-        } else if (conditionedAngle == 135 || conditionedAngle == -225) {
+        } else if ((dropdown.value == "degrees" && (conditionedAngle == 135 || conditionedAngle == -225)) ||
+            (dropdown.value == "piRad") && ((math.equal(conditionedAngle, math.fraction('3/4')))) || math.equal(conditionedAngle, math.fraction('-5/4'))) {
             x = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mrow><mo>-</mo><msqrt><mn>2</mn></msqrt></mrow><mn>2</mn></mfrac></math>`
             y = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><msqrt><mn>2</mn></msqrt><mn>2</mn></mfrac></math>`
             tan = -1
-        } else if (conditionedAngle == 150 || conditionedAngle == -210) {
+        } else if ((dropdown.value == "degrees" && (conditionedAngle == 150 || conditionedAngle == -210)) ||
+            (dropdown.value == "piRad") && ((math.equal(conditionedAngle, math.fraction('5/6')))) || math.equal(conditionedAngle, math.fraction('-7/6'))) {
             x = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mrow><mo>-</mo><msqrt><mn>3</mn></msqrt></mrow><mn>2</mn></mfrac></math>`
             y = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mn>1</mn><mn>2</mn></mfrac></math>`
             tan = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mrow><mo>-</mo><msqrt><mn>3</mn></msqrt></mrow><mn>3</mn></mfrac></math>`
-        } else if (conditionedAngle == 180 || conditionedAngle == -180) {
+        } else if ((dropdown.value == "degrees" && (conditionedAngle == 180 || conditionedAngle == -180)) ||
+            (dropdown.value == "piRad") && ((math.equal(conditionedAngle, math.fraction('1/1')))) || math.equal(conditionedAngle, math.fraction('-1/1'))) {
             x = -1
             y = 0
             tan = 0
-        } else if (conditionedAngle == 210 || conditionedAngle == -150) {
+        } else if ((dropdown.value == "degrees" && (conditionedAngle == 210 || conditionedAngle == -150)) ||
+            (dropdown.value == "piRad") && ((math.equal(conditionedAngle, math.fraction('7/6')))) || math.equal(conditionedAngle, math.fraction('-5/6'))) {
             x = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mrow><mo>-</mo><msqrt><mn>3</mn></msqrt></mrow><mn>2</mn></mfrac></math>`
             y = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mrow><mo>-</mo><mn>1</mn></mrow><mn>2</mn></mfrac></math>`
             tan = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><msqrt><mn>3</mn></msqrt><mn>3</mn></mfrac></math>`
-        } else if (conditionedAngle == 225 || conditionedAngle == -135) {
+        } else if ((dropdown.value == "degrees" && (conditionedAngle == 225 || conditionedAngle == -135)) ||
+            (dropdown.value == "piRad") && ((math.equal(conditionedAngle, math.fraction('5/4')))) || math.equal(conditionedAngle, math.fraction('-3/4'))) {
             x = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mrow><mo>-</mo><msqrt><mn>2</mn></msqrt></mrow><mn>2</mn></mfrac></math>`
             y = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mrow><mo>-</mo><msqrt><mn>2</mn></msqrt></mrow><mn>2</mn></mfrac></math>`
             tan = 1
-        } else if (conditionedAngle == 240 || conditionedAngle == -120) {
+        } else if ((dropdown.value == "degrees" && (conditionedAngle == 240 || conditionedAngle == -120)) ||
+            (dropdown.value == "piRad") && ((math.equal(conditionedAngle, math.fraction('4/3')))) || math.equal(conditionedAngle, math.fraction('-2/3'))) {
             x = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mrow><mo>-</mo><mn>1</mn></mrow><mn>2</mn></mfrac></math>`
             y = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mrow><mo>-</mo><msqrt><mn>3</mn></msqrt></mrow><mn>2</mn></mfrac></math>`
             tan = `<math xmlns="http://www.w3.org/1998/Math/MathML"><msqrt><mn>3</mn></msqrt></math>`
-        } else if (conditionedAngle == 270 || conditionedAngle == -90) {
+        } else if ((dropdown.value == "degrees" && (conditionedAngle == 270 || conditionedAngle == -90)) ||
+            (dropdown.value == "piRad") && ((math.equal(conditionedAngle, math.fraction('3/2')))) || math.equal(conditionedAngle, math.fraction('-1/2'))) {
             x = 0
             y = -1
             tan = "undefined"
-        } else if (conditionedAngle == 300 || conditionedAngle == -60) {
+        } else if ((dropdown.value == "degrees" && (conditionedAngle == 300 || conditionedAngle == -60)) ||
+            (dropdown.value == "piRad") && ((math.equal(conditionedAngle, math.fraction('5/3')))) || math.equal(conditionedAngle, math.fraction('-1/3'))) {
             x = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mn>1</mn><mn>2</mn></mfrac></math>`
             y = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mrow><mo>-</mo><msqrt><mn>3</mn></msqrt></mrow><mn>2</mn></mfrac></math>`
             tan = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mo>-</mo><msqrt><mn>3</mn></msqrt></math>`
-        } else if (conditionedAngle == 315 || conditionedAngle == -45) {
+        } else if ((dropdown.value == "degrees" && (conditionedAngle == 315 || conditionedAngle == -45)) ||
+            (dropdown.value == "piRad") && ((math.equal(conditionedAngle, math.fraction('7/4')))) || math.equal(conditionedAngle, math.fraction('-1/4'))) {
             x = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><msqrt><mn>2</mn></msqrt><mn>2</mn></mfrac></math>`
             y = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mrow><mo>-</mo><msqrt><mn>2</mn></msqrt></mrow><mn>2</mn></mfrac></math>`
             tan = -1
-        } else if (conditionedAngle == 330 || conditionedAngle == -30) {
+        } else if ((dropdown.value == "degrees" && (conditionedAngle == 330 || conditionedAngle == -30)) ||
+            (dropdown.value == "piRad") && ((math.equal(conditionedAngle, math.fraction('11/6')))) || math.equal(conditionedAngle, math.fraction('-1/6'))) {
             x = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><msqrt><mn>3</mn></msqrt><mn>2</mn></mfrac></math>`
             y = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mrow><mo>-</mo><mn>1</mn></mrow><mn>2</mn></mfrac></math>`
             tan = `<math xmlns="http://www.w3.org/1998/Math/MathML"><mfrac><mrow><mo>-</mo><msqrt><mn>3</mn></msqrt></mrow><mn>3</mn></mfrac></math>`
-        }
-        else {
+        } else if (dropdown.value == "degrees") {
             x = +Math.cos(conditionedAngle * Math.PI / 180).toFixed(4)
             y = +Math.sin(conditionedAngle * Math.PI / 180).toFixed(4)
             tan = +Math.tan(conditionedAngle * Math.PI / 180).toFixed(4)
+        } else if (dropdown.value == "radians") {
+            x = +Math.cos(conditionedAngle).toFixed(4)
+            y = +Math.sin(conditionedAngle).toFixed(4)
+            tan = +Math.tan(conditionedAngle).toFixed(4)
+        } else if (dropdown.value == "piRad") {
+            x = +Math.cos(conditionedAngle * Math.PI).toFixed(4)
+            y = +Math.sin(conditionedAngle * Math.PI).toFixed(4)
+            tan = +Math.tan(conditionedAngle * Math.PI).toFixed(4)
         }
         resultElement.style.color = "black";
-        resultElement.innerHTML = `x = cos(${conditionedAngle})° = ${x}\ny = sin(${conditionedAngle})° = ${y}\ntan(${conditionedAngle})° = ${tan}`
-
-    } else if (dropdown.value == "radians") {
-        let conditionedAngle = +(eval(angle) % (2 * Math.PI)).toFixed(3)
-
-        x = +Math.cos(conditionedAngle).toFixed(4)
-        y = +Math.sin(conditionedAngle).toFixed(4)
-        tan = +Math.tan(conditionedAngle).toFixed(4)
-
-        resultElement.style.color = "black";
-        resultElement.innerHTML = `x = cos(${conditionedAngle}) = ${x}\ny = sin(${conditionedAngle}) = ${y}\ntan(${conditionedAngle}) = ${tan}`
-
-    } else if (dropdown.value == "piRad") {
-        //let conditionedAngle = +(eval(angle) % 2 * Math.PI)
-        //let roundedAngle = conditionedAngle.toFixed(3)
-        let conditionedAngle = math.fraction(angle)
-        console.log(conditionedAngle)
-
-        x = +Math.cos(conditionedAngle).toFixed(4)
-        y = +Math.sin(conditionedAngle).toFixed(4)
-        tan = +Math.tan(conditionedAngle).toFixed(4)
-
-        resultElement.style.color = "black";
-        //resultElement.innerHTML = `x = cos(${roundedAngle}) = ${x}\ny = sin(${roundedAngle}) = ${y}\ntan(${roundedAngle}) = ${tan}`
-        resultElement.innerHTML = `${conditionedAngle}`
     }
 
-
-
-
-
 }
 
 
-/*
-        //Intelligent rounding. Results with only decimal component need sig figs, 
-        //results greater than 1 do not
-if (result < 1 && result > -1) {
-    resultElement.textContent = `Answer: ${result.toPrecision(2)} pounds`
-} else {
-    resultElement.textContent = `Answer: ${numberWithCommas(+result.toFixed(2))} pounds`
-}
-resultElement.style.color = "black";
-
-//Format input fields with commas after calculation
-kilogramsInputElement.value = numberWithCommas(kilograms) */
